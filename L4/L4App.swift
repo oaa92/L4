467fdae0d44ec0073493c74bdd687c308bbf2c10
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-09 22:10:17 +0700

    Initial Commit

---
+++ L4/L4App.swift
@@ -0,0 +1,17 @@
+//
+//  L4App.swift
+//  L4
+//
+//  Created by Anatoliy Odinetskiy on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct L4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
