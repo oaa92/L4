ec8825a5ec0d28219e1a33536806007e62ce2624
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-10 20:08:13 +0700

    1

--- L4/ContentView.swift
+++ L4/ContentView.swift
@@ -1,44 +1,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var performAnimation: Bool = false
+    var body: some View {
+        Button {} label: {}
+        .buttonStyle(PlayNextButtonStyle())
+    }
+}
+
+struct BackgroundButtonStyle: ButtonStyle {
+    @Binding var performAnimation: Bool
+
+    func makeBody(configuration: Configuration) -> some View {
+        let scale: Double = performAnimation ? 0.86 : 1
+        let duration = 0.22
+        let opacity: Double = performAnimation ? 0.25 : 0
+
+        configuration.label
+            .animation(.linear(duration: duration)) {
+                $0
+                    .background() {
+                        Circle()
+                            .fill(Color.secondary)
+                            .frame(width: 100, height: 100)
+                            .opacity(opacity)
+                    }
+                    .scaleEffect(scale)
+            }
+    }
+}
+
+struct PlayNextButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        ButtonView(performAnimation: configuration.isPressed)
+    }
+}
+
+struct ButtonView: View {
+    @State var performAnimation = false
 
     var body: some View {
-        Button {
-            withAnimation(.interpolatingSpring(stiffness: 170, damping: 15)){
-                performAnimation.toggle()
-            }
-            completion: {
-                performAnimation.toggle()
-            }
-        } label: {
-            GeometryReader(content: { geometry in
-                let width = geometry.size.width / 2
-
-                HStack(alignment: .center, spacing: 0) {
-                    Image(systemName: "play.fill")
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: performAnimation ? width : .zero)
-                        .opacity(performAnimation ? 1 : .zero)
-
-                    Image(systemName: "play.fill")
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-
-                    Image(systemName: "play.fill")
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: performAnimation ? 0.5 : width)
-                        .opacity(performAnimation ? .zero : 1)
-                }
-                .frame(maxHeight: .infinity, alignment: .center)
-            })
+        Button {withAnimation(.interpolatingSpring(stiffness: 170, damping: 15)) {
+            performAnimation.toggle()
+        } completion: {
+            performAnimation.toggle()
+        }}
+        label: {
+            PlayImagesView(performAnimation: $performAnimation)
+                .frame(maxWidth: 64, alignment: .center)
         }
-        .frame(maxWidth: 62)
+        .buttonStyle(BackgroundButtonStyle(performAnimation: $performAnimation))
+    }
+}
+
+struct PlayImagesView: View {
+    @Binding var performAnimation: Bool
+    let width: Double = 32
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 0) {
+            PlayImage()
+                .frame(width: performAnimation ? width : .zero)
+                .opacity(performAnimation ? 1 : .zero)
+
+            PlayImage()
+
+            PlayImage()
+                .frame(width: performAnimation ? 0.5 : width)
+                .opacity(performAnimation ? .zero : 1)
+        }
+    }
+}
+
+struct PlayImage: View {
+    var body: some View {
+        Image(systemName: "play.fill")
+            .renderingMode(.template)
+            .resizable()
+            .scaledToFit()
+            .foregroundStyle(Color.accentColor)
     }
 }
 
